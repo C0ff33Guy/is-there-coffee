5a6cfb7fa4b3a57a97166e0621488c38b68d9805
CoffeeGuy <vessel-snow-0n@icloud.com>
2023-11-29 11:33:02 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const app = express();
+
+const expressWs = require('express-ws')(app);
+
+const status = {
+  state: null,
+  updatedAt: new Date,
+}
+
+app.use(express.static('public'));
+
+function broadcast(data) {
+  expressWs.getWss().clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  })
+}
+
+function state() {
+  if (status.updatedAt.getDate() != (new Date).getDate()) {
+    status.state = null;
+    status.updatedAt = new Date();
+  }
+
+  return JSON.stringify({
+    state: status.state,
+    updatedAt: status.updatedAt.toISOString(),
+  })
+}
+
+app.ws('/ws', (ws, req) => {
+  ws.send(state())
+  ws.on('message', (msg) => {
+    if (!['true', 'false'].includes(msg.toString())) return;
+    const val = JSON.parse(msg);
+
+    status.state = val;
+    status.updatedAt = new Date();
+    console.log(status.updatedAt, val)
+
+    broadcast(state());
+  })
+})
+
+app.listen(process.env.PORT || 80);
+console.log('listening');
